98828cbe393bfabed7363242f5d518bd298688b4
Jiawei Lu <jl5999@columbia.edu>
2022-12-15 22:22:08 -0500

    homepage slides

---
+++ src/main/java/edu/columbia/cosmos/controller/ToPageController.java
@@ -0,0 +1,34 @@
+package edu.columbia.cosmos.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToPageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/documentation")
+    public String documentation() {
+        return "documentation";
+    }
+
+    @GetMapping("/resource")
+    public String resource() {
+        return "resource";
+    }
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
